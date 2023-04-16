f2f82c69017a13962d994013ef827300c207937c
escidron <eduardosanchezcidron@gmail.com>
2023-04-15 21:30:00 -0300

    add faviicon and portafolio title

--- pages/[firstId]/details/[detailsId].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router"
-
-export default function DetailsId() {
-    const router = useRouter()
-    const detailId = router.query.detailsId
-  return (
-    <div>{detailId}</div>
-  )
-}
