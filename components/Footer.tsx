e45cb90f986f75c27f252e2f2e4321dc3cf0758e
escidron <eduardosanchezcidron@gmail.com>
2023-04-15 21:12:41 -0300

    add logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import Image from 'next/image'
+import logo from '../public/ES_LOGO.jpg'
 
 export default function Footer() {
   return (
     <div className='border-t border-gray-400 h-[80px] flex items-center justify-center bg-[#10101a]' >
-        <div className='mx-auto  my-auto w-[200px] flex flex-col items-center justify-between'>
+        <div className='h-[50px] w-[50px]'>
+
+            <Image
+              src={logo}
+              alt='logo'
+              className="w-full rounded-[8px]  
+            "
+            />
+            </div>
+        <div className=' my-auto w-[200px] flex flex-col items-center justify-between'>
             <h1 className='text-[#7de24e] font-medium text-xl'>Eduardo Sanchez</h1>
             <h1 className='text-[#ebe7d7ea] font-medium text-[12px]'>Software Developer</h1>
         </div>
