5f42ac089b7a8de3981ab0aade7780fd869fced6
escidron <eduardosanchezcidron@gmail.com>
2023-04-26 23:21:56 -0300

    impruving SEO

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -22,8 +22,8 @@ export default function NavBar() {
        "
       />
     </div>
-   <Link activeClass="active" to="#"  smooth={true} offset={50} duration={300} className='text-[#ebe7d7ea]  md:text-lg font-medium ml-5'><h1 className='links'>Home</h1></Link>
-   <Link activeClass="active" to="projects"  smooth={true} offset={-10} duration={300} className='text-[#ebe7d7ea]  md:text-lg font-medium ml-8'><h1 className='links'>Projects</h1></Link>
+   <Link activeClass="active" href = '#' to="#"  smooth={true} offset={50} duration={300} className='text-[#ebe7d7ea]  md:text-lg font-medium ml-5'><h1 className='links'>Home</h1></Link>
+   <Link activeClass="active" href = 'projects' to="projects"  smooth={true} offset={-10} duration={300} className='text-[#ebe7d7ea]  md:text-lg font-medium ml-8'><h1 className='links'>Projects</h1></Link>
    <Link activeClass="active" to="contact"  smooth={true} offset={50} duration={300} className='text-[#ebe7d7ea]  md:text-lg font-medium ml-8'><h1 className='links'>Contact</h1></Link>
    <Link activeClass="active" to="about"  smooth={true} offset={-10} duration={300} className='text-[#ebe7d7ea]  md:text-lg font-medium ml-8'><h1 className='links'>About me</h1></Link>
 
