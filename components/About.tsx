81efc680bb8b0a1d767935463896a695053b697e
escidron <eduardosanchezcidron@gmail.com>
2024-02-09 12:18:30 -0300

    update about me section

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className='h-auto px-[30px] md:px-[60px] lg:px-[200px] 2lg:py-[100px] w-full xl:w-[80%]' id='about'>
         <h1 className='text-[#ebe7d7ea] font-medium text-3xl md:text-4xl'>About me</h1>
-        <h4 className='2lg:w-1/2 text-[#ebe7d7ea] mt-4 font-sans leading-8 text-lg md:text-xl'>Dedicated Front-End developer with 5 years of experience working with a variety of technologies as HTML,CSS,JavaScript,React,Node.js to build and maintain full Web Applications. Passionate about knowledge and personal improvement, I am able to work with new technologies and work on multiple projects at the same time,consistently achieving high quality results, performance, and positive feedback.</h4>
+        <h4 className='2lg:w-1/2 text-[#ebe7d7ea] mt-4 font-sans leading-8 text-lg md:text-xl'>Dedicated Software Enginner with 5 years of experience working with a variety of technologies as React,Next.js,Node.js,Grphql to build and maintain full Web Applications. Passionate about knowledge and personal improvement, I am able to work with new technologies and work on multiple projects at the same time,consistently achieving high quality results, performance, and positive feedback.</h4>
     </div>
   )
 }
